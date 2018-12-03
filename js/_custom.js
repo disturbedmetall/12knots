58c21129066531d816a9a4abf645da3aea546279
Alex <disturbedmetall@gmail.com>
2018-12-03 21:40:41 +0200

    roboto font and touch pills

--- js/_custom.js
+++ js/_custom.js
@@ -1,5 +1,25 @@
 ;
 jQuery(document).ready(function($) {
+  var ourOfficeIcon = L.icon({
+    iconUrl: '../images/location.svg',
+    iconSize: [40, 40],
+    iconAnchor: [20, 40],
+    popupAnchor: [0, -40]
+  });
+
+  var ourOffice = L.marker([55.7646756, 37.5951047], {icon: ourOfficeIcon});
+
+  var map = L.map('map', {
+    center: [55.7646756, 37.5951047],
+    zoom: 15,
+      layers: [ourOffice]
+  });
+
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+    maxZoom: 18
+}).addTo(map);
+
   feather.replace()
 
   $('.reviews_slider').slick({
@@ -36,24 +56,5 @@ jQuery(document).ready(function($) {
       }
     ]
   });
-
-
-  var ourOfficeIcon = L.icon({
-    iconUrl: '../images/location.svg',
-    iconSize: [40, 40],
-    iconAnchor: [20, 40],
-    popupAnchor: [0, -40]
-  });
-
-  var ourOffice = L.marker([55.7646756, 37.5951047], {icon: ourOfficeIcon});
-
-  var map = L.map('map', {
-    center: [55.7646756, 37.5951047],
-    zoom: 15,
-      layers: [ourOffice]
-  });
-
-  L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  }).addTo(map);
 
 })(jQuery);
