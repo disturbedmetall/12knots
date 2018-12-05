6619bc1b6802257178a0a5eaef9227ab22042ae4
Alex <disturbedmetall@gmail.com>
2018-12-05 13:27:25 +0200

    slick autoplay

--- js/_custom.js
+++ js/_custom.js
@@ -1,38 +1,20 @@
 ;
-jQuery(document).ready(function($) {
-  var ourOfficeIcon = L.icon({
-    iconUrl: '../images/location.svg',
-    iconSize: [40, 40],
-    iconAnchor: [20, 40],
-    popupAnchor: [0, -40]
-  });
-
-  var ourOffice = L.marker([55.7646756, 37.5951047], {icon: ourOfficeIcon});
-
-  var map = L.map('map', {
-    center: [55.7646756, 37.5951047],
-    zoom: 15,
-      layers: [ourOffice]
-  });
-
-  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-    maxZoom: 18
-}).addTo(map);
-
-  feather.replace()
+(function() {
 
   $('.reviews_slider').slick({
     centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 3,
+    slidesToShow: 4,
     arrows: true,
+    autoplay: true,
     responsive: [{
-        breakpoint: 992,
+        breakpoint: 1440,
         settings: {
-          arrows: true,
-          centerMode: true,
-          centerPadding: '40px',
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 1100,
+        settings: {
           slidesToShow: 2
         }
       },
@@ -41,29 +23,54 @@ jQuery(document).ready(function($) {
         settings: {
           arrows: false,
           centerMode: false,
-          centerPadding: '40px',
           slidesToShow: 2
         }
       },
       {
-        breakpoint: 552,
+        breakpoint: 576,
         settings: {
           arrows: false,
           centerMode: true,
-          centerPadding: '40px',
           slidesToShow: 1
         }
       }
     ]
   });
+})();
+
+jQuery(document).ready(function($) {
+  var ourOfficeIcon = L.icon({
+    iconUrl: '../images/location.svg',
+    iconSize: [40, 40],
+    iconAnchor: [20, 40],
+    popupAnchor: [0, -40]
+  });
+
+  var ourOffice = L.marker([55.7646756, 37.5951047], {
+    icon: ourOfficeIcon
+  });
+
+  var map = L.map('map', {
+    center: [55.7646756, 37.5951047],
+    zoom: 15,
+    layers: [ourOffice]
+  });
+
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+    maxZoom: 18
+  }).addTo(map);
+
+  feather.replace()
 
   var kLang = document.getElementById("k-lang");
   kLang.addEventListener("click", toggleIcon, false);
-  function toggleIcon(){
+
+  function toggleIcon() {
     if (this.classList.contains('flag--russia')) {
       this.classList.remove("flag--russia");
       this.classList.add("flag--usa");
-    } else if(this.classList.contains('flag--usa')){
+    } else if (this.classList.contains('flag--usa')) {
       this.classList.remove("flag--usa");
       this.classList.add("flag--russia");
     } else {
